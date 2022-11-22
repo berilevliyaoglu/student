29884dc263f16c98dadc0e16c1ebef5e5ed35a72
berilevliyaoglu <76916613+berilevliyaoglu@users.noreply.github.com>
2022-11-22 15:23:09 +0300

    configuration class added

--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -20,4 +20,10 @@ public class StudentService {
     public List<Student> getStudents(){
         return studentRepository.findAll();
     }
+
+    public void addNewStudent(Student student) {
+        System.out.println(student);
+
+
+    }
 }
